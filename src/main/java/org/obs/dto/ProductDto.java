f970f97d654fa344cc2b668a2bba212564a5eee5
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-23 10:58:37 +0100

    add ProductService and ProductDto

--- src/main/java/org/obs/dto/ProductDto.java
+++ src/main/java/org/obs/dto/ProductDto.java
@@ -1,4 +1,37 @@
 package org.obs.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.obs.model.ProductCategory;
+import org.obs.model.ProductStatus;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
+
+    private UUID id;
+    private String sku;
+    private String title;
+    private String description;
+    private String image;
+    private double price;
+    private String manufacturer;
+    private String seller;
+    private int quantityStock;
+
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
+    
 }
--- src/main/java/org/obs/service/ProductService.java
+++ src/main/java/org/obs/service/ProductService.java
@@ -1,4 +1,17 @@
 package org.obs.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+import org.obs.repository.ProductRepository;
+
+@ApplicationScoped
+@AllArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+
+
+
 }
