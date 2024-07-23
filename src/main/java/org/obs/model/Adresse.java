fb03c6b0d62e7894081919c2f2838632551ca30d
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-23 14:24:27 +0100

    add more fields

--- src/main/java/org/obs/model/Adresse.java
+++ src/main/java/org/obs/model/Adresse.java
@@ -22,14 +22,18 @@ public class Adresse {
     private UUID id;
 
     @Column(name = "street_number", nullable = false, length = 50)
-    private String streetNumber;
+    private int streetNumber;
+
+    @Column(name = "street_name", nullable = false, length = 50)
+    private String streetName;
 
     @Column(name = "city", nullable = false, length = 50)
     private String city;
 
     @Column(name = "zip_code", nullable = false, length = 50)
-    private String zipCode;
+    private int zipCode;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "country", nullable = false, length = 50)
     private CountryCode country;
 }
