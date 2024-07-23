cd936a5c9790286dcec8ef4b782806da36b0d3f4
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-23 10:52:32 +0100

    add product repository

--- src/main/java/org/obs/repository/ProductRepository.java
+++ src/main/java/org/obs/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package org.obs.repository;
 
-public class ProductRepository {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+import org.obs.model.Product;
+
+@ApplicationScoped
+@AllArgsConstructor
+public class ProductRepository implements PanacheRepository<Product> {
+    
 }
