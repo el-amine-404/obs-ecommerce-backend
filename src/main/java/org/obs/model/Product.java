49781250141f1f853609a77f191d18dbe18088f0
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-29 09:52:27 +0100

    remove all length constraints

--- src/main/java/org/obs/model/Product.java
+++ src/main/java/org/obs/model/Product.java
@@ -17,36 +17,36 @@ public class Product {
     @Column(name = "product_id")
     private Long id;
 
-    @Column(name = "sku", nullable = false, length = 50)
+    @Column(name = "sku", nullable = false)
     private String sku; // Stock Keeping Unit (provided by the manufacturer)
 
-    @Column(name = "title", nullable = false, length = 50)
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "description", nullable = false, length = 50)
+    @Column(name = "description", nullable = false)
     private String description;
 
-    @Column(name = "image", nullable = false, length = 50)
+    @Column(name = "image", nullable = false)
     private String image;
 
-    @Column(name = "price", nullable = false, length = 50)
+    @Column(name = "price", nullable = false)
     private double price;
 
-    @Column(name = "manufacturer", nullable = false, length = 50)
+    @Column(name = "manufacturer", nullable = false)
     private String manufacturer;
 
-    @Column(name = "seller", nullable = false, length = 50)
+    @Column(name = "seller", nullable = false)
     private String seller;
 
-    @Column(name = "quantity_stock", nullable = false, length = 50)
+    @Column(name = "quantity_stock", nullable = false)
     private int quantityStock;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "category", nullable = false, length = 50)
+    @Column(name = "category", nullable = false)
     private ProductCategory category;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, length = 50)
+    @Column(name = "status", nullable = false)
     private ProductStatus status;
 
 //    @OneToOne(mappedBy = "product")
