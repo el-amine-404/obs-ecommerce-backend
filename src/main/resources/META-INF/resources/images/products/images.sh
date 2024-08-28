2573e4b10db4b8bd34a467676987dde1173d103c
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-28 14:00:50 +0100

    add images of sample products

---
+++ src/main/resources/META-INF/resources/images/products/images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -O logitech_m185.jpg https://www.atacbureau.com/web/image/product.template/6188/image
+wget -O dell_xps_13.jpg https://micromad.ma/wp-content/uploads/2023/02/Dell-xps-13-Plus-9320-yaratech.ma_-scaled.webp
+wget -O kingston_ddr4_32gb.jpg https://img.telemart.ua/374728-523065-product_popup/kingston-ddr4-32gb-2x16gb-3200mhz-fury-beast-black-kf432c16bbk232.jpg
+wget -O hp_m404dn.jpg https://moumtaz.ma/wp-content/uploads/2021/11/Laser-Jet-Pro-M404dn-B.jpg
+wget -O epson_v600.jpg https://tabtel.ma/8332-thickbox_default/scanner-epson-v600-photo-perfection-b11b198032.jpg
+wget -O canon_eos_r6.jpg https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrSbJmztpZeouRWZkPFjR6VRW3FtJEm4c6ysk7qEMkOxlSqiNslr__FTZSzYxjofN0Dmo
+wget -O office_365.jpg https://www.grafe.fr/wp-content/uploads/2020/09/Integrateur-Office-365.png
+wget -O imac_24.jpg https://uno.ma/pub/media/catalog/product/cache/af8d7fd2c4634f9c922fba76a4a30c04/i/m/imacs1.jpeg
+wget -O galaxy_s23.jpg https://images.samsung.com/is/image/samsung/p6pim/n_africa/2302/gallery/n-africa-galaxy-s23-s911-sm-s911blicmwd-534844949?$650_519_PNG$
+wget -O ipad_pro_11.jpg https://uno.ma/pub/media/catalog/product/cache/af8d7fd2c4634f9c922fba76a4a30c04/l/d/ld0005988844_0005988849_0005988859_0005988864_4.jpeg
